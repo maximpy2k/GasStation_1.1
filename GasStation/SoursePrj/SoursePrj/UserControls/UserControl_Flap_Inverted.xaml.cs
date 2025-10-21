a65498688c1ece5db63183453b7a15af0967b480
ivanovda <ivanov_nox@mail.ru>
2025-10-21 12:01:19 +0300

    Исправлены константы Добавлен инвертированный датчик

---
+++ GasStation/SoursePrj/SoursePrj/UserControls/UserControl_Flap_Inverted.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using GasStation.ModalWindows.View;
+using GasStation.xml.Script;
+
+namespace SoursePrj
+{
+    /// <summary>
+    /// Interaction logic for UserControl_Flap.xaml
+    /// </summary>
+    public partial class UserControl_Flap_Inverted : UserControl
+    {
+        public UserControl_Flap_Inverted()
+        {
+            InitializeComponent();         
+        }
+
+        private void MenuParams(object sender, RoutedEventArgs e)
+        {
+            var win = new WindowFlapParams();
+            if (DataContext == null)
+            {
+                return;
+            }
+
+            var flap = (XmlClassFlap)DataContext;
+
+            win.DataContext = flap;
+            win.ShowDialog();
+        }
+
+    }
+}
